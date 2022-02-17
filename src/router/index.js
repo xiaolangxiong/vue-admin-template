d397a192160dfcc491f4339d9573593d9858c32d
笔记本 <13719343938@163.com>
2022-02-17 16:27:21 +0800

    提交代码

--- src/router/index.js
+++ src/router/index.js
@@ -170,7 +170,8 @@ export const asyncRoutes = [
 ]
 
 const createRouter = () => new Router({
-  // mode: 'history', // require service support
+  mode: 'history', // require service support
+  base: '/myblog',
   scrollBehavior: () => ({ y: 0 }),
   routes: constantRoutes
 })
